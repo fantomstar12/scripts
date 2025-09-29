485b426011b372d9b30091460bf2b7dc60cebb5e
fantomstar12 <ioffical02@gmail.com>
2025-09-30 02:16:20 +0400

    Update and rename CameraManager.lua to CurrencyManager.lua

--- ModuleScripts/CameraManager.lua
+++
@@ -1,6 +0,0 @@
-local CameraManager = {}
-
-
-
-
-return CameraManager
---
+++ ModuleScripts/CurrencyManager.lua
@@ -0,0 +1,36 @@
+local CurrencyManager = {}
+
+-- increases
+function CurrencyManager.AddCurrency(player: Player, Amount ,Currency: String)
+local leaderstats = player:FindFirstChild("leaderstats")
+local currency = leaderstats.Currency
+if not currency then return false end
+if not leaderstats then return false end
+
+if currency then
+currency.Value += amount
+    return true
+  end    
+
+end 
+
+-- Gets currency.value
+function CurrencyManager.GetCurrency(player: Player, Currency: String)
+
+  return player.leaderstats.Currency.Value
+
+end 
+
+-- Sets the currency
+function CurrencyManager.SetCurrency(player: Player, Amount, Currency: String)
+
+local currency = player.leaderstats:FindFirstChild(Currency)
+if not currency then return false
+
+    currency.Value = amount
+    return true
+
+
+end
+
+return CurrencyManager
