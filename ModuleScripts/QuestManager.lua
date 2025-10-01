2d695eb29ec685acf35a4cb78872793371acc054
fantomstar12 <ioffical02@gmail.com>
2025-10-01 06:06:12 +0400

    Update QuestManager.lua

--- ModuleScripts/QuestManager.lua
+++ ModuleScripts/QuestManager.lua
@@ -1,18 +1,30 @@
 local QuestHandler = {}
-
+local playerQuestData = {}
 local Quests = {
   Quest1 = {
+    QuestID = "1Q",
     QuestName = "Defeat 5 NPCs.",
     KillsNeeded = 5,
-    Have = 0,
     Done = false,
-  },
-  Quest2 = {}
+  }
 }
 
 function QuestHandler.StartQuest(player: Player, quest)
 
-
+local playerUserId = player.UserId
+  if not playerQuestData[playerUserId] then
+    playerQuestData[playerUserId] = {
+    ActiveQuests = {},
+    CompletedKills = {},
+    CompletedQuests = {},
+    }
+  end
+  local quest = Quests[QuestID]
+  if not quest then
+    print("Quest not found!")
+    return false
+--- more yet to come....
+    
 end  
 
 return QuestHandler
