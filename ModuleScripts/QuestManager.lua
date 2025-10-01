6a32a646aeb7035cdbf9b424ac4351b17ded4fa1
fantomstar12 <ioffical02@gmail.com>
2025-10-01 05:58:13 +0400

    Create QuestManager.lua

---
+++ ModuleScripts/QuestManager.lua
@@ -0,0 +1,18 @@
+local QuestHandler = {}
+
+local Quests = {
+  Quest1 = {
+    QuestName = "Defeat 5 NPCs.",
+    KillsNeeded = 5,
+    Have = 0,
+    Done = false,
+  },
+  Quest2 = {}
+}
+
+function QuestHandler.StartQuest(player: Player, quest)
+
+
+end  
+
+return QuestHandler
